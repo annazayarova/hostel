ff11e136f822d6717280a4d1eed045b021d6c38f
Anna Zayarova <annazayarova@Annas-MacBook-Pro.local>
2020-05-09 23:02:30 +0300

    first commit

---
+++ src/common/ItemList.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import Item from './Item';
+
+const ItemList = ({ className, items }) => {
+    return (
+        <Root className={ className }>
+            { items.map(item => (
+                <Item key={ item }
+                    item={ item }
+                />
+            )) }
+        </Root>
+    );
+};
+
+export default ItemList;
+
+const Root = styled.div`
+    border-top: 1px solid black;
+    position: relative;
+    cursor: pointer;
+`;
---
+++ src/pages/Home.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Logo from '../common/Logo';
+import Social from '../common/Social';
+import Transition from '../common/Transition'
+
+const Home = () => {
+    return (
+        <Root id="home">
+            <Logo />
+
+            <TopTransition text="backpakers" />
+
+            <BottomTransition text="sunny limassol cyprus" />
+
+            <MiddleTransition fontSize='26px'
+                mobileFontSize='12px'
+                transform="inherit"
+                text="Make yourself at home. Mi casa es su casa. Sentitevi come a casa vostra. Fais comme chez toi. Sinta-se em casa. Fühle sie sich wie zu hause."
+            />
+
+            <Social />
+        </Root>
+    );
+}
+
+export default Home;
+
+const TopTransition = styled(Transition)`
+    position: absolute;
+    top: 0;
+    color: ${ props => props.theme.primaryColorPelette.light };
+    font-weight: bold;
+`;
+
+const BottomTransition = styled(Transition)`
+    position: absolute;
+    bottom: 0;
+    color: ${ props => props.theme.primaryColorPelette.light };
+    font-family: ${ props => props.theme.fontFamily.secondary };
+`;
+
+const MiddleTransition = styled(Transition)`
+    position: absolute;
+    top: 50%;
+    height: 20px;
+    line-height: 20px;
+    margin-top: -47px;
+    color: white;
+    font-family: ${ props => props.theme.fontFamily.secondary };
+    text-transform: uppercase;
+
+    @media(max-width: ${ ({ theme }) => theme.tabletBreakpoint }) {
+        margin-top: -33px;
+    }
+`;
+
+const Root = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    height: 100%;
+    width: 100%;
+`;
